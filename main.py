3cb1ea905080cb1bcda2f9293fca6f7b3067fc81
FabioCarusi <carusi@inwind.it>
2023-08-07 17:10:50 +0200

    add comments

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
-
+# import libraries
 import requests
-
 from bs4 import BeautifulSoup as BS
-from webscraping import *  
 from flask import Flask, request
 
+# import module
+from webscraping import *
+
+# define Flask app
 app = Flask(__name__)
 
-@app.route('/operator')
-def get_operator(): 
+# define GET method
+
+
+@app.route('/operator', methods=["GET"])
+def get_operator() -> list:
     operator = request.args['operator']
     operator = operator.lower()
-    URL = 'https://www.ubisoft.com/en-us/game/rainbow-six/siege/game-info/operators/{}'.format(operator)
-    print(URL)
+    URL = 'https://www.ubisoft.com/en-us/game/rainbow-six/siege/game-info/operators/{}'.format(
+        operator)
     page = requests.get(URL)
     soup = BS(page.content, 'html.parser')
-    return export_json(soup, operator)
+    return export_json(soup)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
-
-    
-
-
+    app.run(debug=False, port=5000)
